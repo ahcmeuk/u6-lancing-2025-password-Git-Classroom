0ddaaef97a4e5ccd702374448cbd2c454bfee2ef
Daniil <dmorozkin26@gmail.com>
2025-02-26 17:31:34 +0000

    semi did the program

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func Valid(password string) {
+	not_vld := true
+	var pas string
+	for not_vld {
+		fmt.Println("Enter your password")
+		fmt.Print("-> ")
+		fmt.Scan(&pas)
+		if pas == password {
+			fmt.Println("nice")
+			not_vld = false
+		}
+	}
+	fmt.Println("Cusseccfully logged in!")
+}
 
 func main() {
-	fmt.Println("you didnt actaully seta  task")
-	fmt.Println("so i wrote a prog in go, haha")
+	var password string
+	fmt.Println("your username is - user")
+	fmt.Println("create your password")
+	fmt.Print("-> ")
+	fmt.Scan(&password)
+	Valid(password)
+	fmt.Println()
+	fmt.Println("also - easier than easy")
 }
